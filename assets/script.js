50ff05ca169e1d26d56b97f2a385d9208fb282cd
unknown <joseph.mickk@gmail.com>
2022-06-15 22:06:40 +0200

    next

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,89 @@
-let h2 = document.querySelector("h2");
-let reponses = document.querySelectorAll(".reponses");
-let rep1 = document.getElementById("rep1");
-let rep2 = document.getElementById("rep2");
-let rep3 = document.getElementById("rep3");
-let rep4 = document.getElementById("rep4");
-
-const data = [
+let data = [
     {
         question: "Quel pays est associé au samourai",
-        choices: ["La suede", "l'inde", "chine", "japon"],
+        a: "La suede",
+        b: "l'inde",
+        c: "chine",
+        d:"japon",
         correct: "japon",
-    active: false
+        active: true
 },
 {
     question: "Quel est la capital de la France",
-    choices: ["Paris", "New york", "Barcelone", "la réponse d"],
+    a: "Paris",
+    b: "New york",
+    c: "Barcelone",
+    d:"la réponse d",
     correct: "Paris",
     active: true
 },
 {
     question: "La fin du monde est ...",
-    choices: ["Inevitable", "un projet", "une fiction", "zombieee"],
+    a: "Inevitable",
+    b: "un projet",
+    c: "une fiction",
+    d:"zombieee",
     correct: "zombieee",
     active: true
 },
 ];
-let answers = []
-//let goodAnswers = ["japon","Paris","Zombie"]
-let obj_actu = data[0];
-console.log(data);
-console.log(obj_actu);
 
-h2.innerText = data[0].question;
-rep1.innerHTML = data[0].choices[0];
-rep2.innerHTML = data[0].choices[1];
-rep3.innerHTML = data[0].choices[2];
-rep4.innerHTML = data[0].choices[3];
+
+let question = document.getElementById("question");
+let rep1 = document.getElementById("rep1");
+let rep2 = document.getElementById("rep2");
+let rep3 = document.getElementById("rep3");
+let rep4 = document.getElementById("rep4");
+const table = [rep1, rep2, rep3, rep4]
+
+console.log(data);
+
 
 function loadQuestion() {
-    console.log(data.length);
-  for (i = 0; i < data.length; i++) {
-      if (data[i].active == true){
-          obj_actu = data[i];
-          h2.innerText = data[i].question;
-          rep1.innerHTML = data[i].choices[0];
-          rep2.innerHTML = data[i].choices[1];
-          rep3.innerHTML = data[i].choices[2];
-          rep4.innerHTML = data[i].choices[3];
-          data[i].active = false
-          //console.log(h2)
+
+    for (const iterator of data) {
+        if (iterator.active === true){
+          console.log(iterator.active);
+          question.innerText = iterator.question;
+          rep1.innerText = iterator.a;
+          rep2.innerText = iterator.b;
+          rep3.innerText = iterator.c;
+          rep4.innerText = iterator.d;
+          iterator.active = false;
+          return;
         }
+    }
+};
+loadQuestion()
+
+function checked (){
+    let checked = 0;
+    for(const iterator of data){
+        if(iterator.active === true){
+            checked = 0;
+        }else{
+            checked= 1;
+        }
+    }
+    return checked;
+};
+
+function next (){
+    let value = checked();
+    console.log(value)
+    if(value === 0){
+        loadQuestion();
     }
 }
 
-//console.log(reponses);
-for (const iterator of reponses) {
-    console.log(iterator)
-    iterator.addEventListener("click", function (e) {
-        console.log(e);
-        let rep = e.target.outerText
-        loadQuestion();
-    answers.push(rep)
-    // console.log(rep);
-    console.log(answers);
-});
-}
+for (const iterator of table) {
+    iterator.addEventListener("click", next);
+  }
 
-
-
+//   function next2(){
+//     for(const iterator of data){
+//         if(iterator.active === true){
+//             loadQuestion()
+//         }
+//     }
+//   }
